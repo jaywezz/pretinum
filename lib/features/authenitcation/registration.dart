aa74eb563d4e618f45ea3f0906b024179b65cefe
James <wawerujaymo92@gmail.com>
2025-06-04 22:16:51 +0300

    another change

--- lib/features/authenitcation/registration.dart
+++ lib/features/authenitcation/registration.dart
@@ -44,7 +44,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 ),
                 const SizedBox(height: 8),
                 Text(
-                  'Simplify your crypto payments with us',
+                  'Simplify your crypto payments with us today',
                   style: theme.textTheme.bodyLarge?.copyWith(
                     color: theme.colorScheme.onSurface.withOpacity(0.3),
                     fontWeight: FontWeight.bold,
